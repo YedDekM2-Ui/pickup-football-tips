841d7e0387586c03414bbe6ad925cad46505653d
t.seeedz <t.seeedz@gmail.com>
2026-08-27 01:16:01 +0700

    อัปรูปบิลจากอัลบั้มได้ (เลิกบังคับเปิดกล้อง) + เวลาเตะจากฟีดจับชื่อทีมคนละแบบได้

--- js/page-mybet.js
+++ js/page-mybet.js
@@ -129,8 +129,8 @@ function betFormHtml() {
 
   return '<div class="bf">' +
     '<div class="bf-head">ลงบิลใหม่</div>' +
-    '<label class="bf-shot">📷 ถ่ายรูปสลิป / เลือกรูป' +
-      '<input type="file" accept="image/*" capture="environment" onchange="betFormShot(this)">' +
+    '<label class="bf-shot">🖼 เลือกรูปบิลจากอัลบั้ม' +
+      '<input type="file" accept="image/*" onchange="betFormShot(this)">' +
     '</label>' +
     '<label class="bf-row"><span class="bf-k">ตลาด</span>' +
       '<select class="bf-i" onchange="betFormSet(\'ตลาด\', this.value)">' +
